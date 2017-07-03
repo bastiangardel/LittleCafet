92abf5ac0284866e866d7f938b68a4d34aab8a28
Bastian Gardel <bastiangardel@gmail.com>
2017-07-03 11:01:29 +0200

    initial commit 2

---
+++ src/main/java/ch/littlecafet/server/LittlecafetApplication.java
@@ -0,0 +1,12 @@
+package ch.littlecafet.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LittlecafetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LittlecafetApplication.class, args);
+	}
+}
